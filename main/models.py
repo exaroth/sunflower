0e9616a8c7de70755ee80e3df9d8091adf2479e4
Konrad Wąsowicz <exaroth@gmail.com>
2014-07-28 05:33:24 +0200

    Added pillow to requirements

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class UserExt(models.Model):
+
+    user = models.OneToOneField(User)
+
+    portrait = models.ImageField(upload_to="profile_images")
