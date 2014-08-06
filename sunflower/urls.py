8aee4871db57cc367e2f5721bfae9bcef75b86ed
Konrad Wąsowicz <exaroth@gmail.com>
2014-08-06 19:20:12 +0200

    Added account creation screen

--- sunflower/urls.py
+++ sunflower/urls.py
@@ -4,7 +4,10 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
-from core.cbviews import IndexView
+from core.cbviews import ( IndexView,
+                          RedirectIndexView,
+                         CreateAccountView
+                         )
 
 urlpatterns = patterns('',
                        # Examples:
@@ -12,6 +15,8 @@ urlpatterns = patterns('',
                        # url(r'^blog/', include('blog.urls')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r"^$", IndexView.as_view(), name="index"),
+                       url(r"^index/$", RedirectIndexView.as_view(), name="index_redir"),
+                       url(r"^account_create/$", CreateAccountView.as_view(), name="account_create"),
                       )
 
 if settings.DEBUG:
