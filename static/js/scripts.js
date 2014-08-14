2a7ce74e5eaf52999ea06949befe82b3c4d34c93
Konrad Wąsowicz <exaroth@gmail.com>
2014-08-15 00:12:45 +0200

    Addedajax request when scrolled to bottom

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -4,21 +4,24 @@
 
 	var $document = $(document),
 		$window = $(window),
-		$images_list_container = $("#index-images-list")
+		$indexImageGrid = $("#index-image-grid"),
+		images_per_page = 10,
+		page = 1,
+		bottom = false
 
 	var images_list,
 		number_of_images,
-		images_count
+		images_count,
+		images_shown
 
 	// Templates
-	var imageItemTemplate = "<li><img src='{{ thumb }}' title='{{ title  }}' class='index-image-item'></li>"
+	var imageItemTemplate = "<img src='{{ thumb }}' title='{{ title  }}' class='index-image-item' data-img-href={{img}}>"
+
 	$document.ready(function(){
 		init();
-	})
+	});
 
 	function init(){
-
-
 		$window.load(function(){
 			main();
 		});
@@ -26,29 +29,45 @@
 	};
 
 	function main(){
-		console.log("running main function")
+		$window.scroll(function(){
+			if($window.scrollTop() + $window.height() == $document.height()){
+				if (!bottom){
+					page += 1;
+					getImages(page, appendIndexImages);
+				}
+			}
+		})
+		images_shown = images_per_page;
 		getImages(1, appendIndexImages);
 	};
 
 	function getImages(page, successFunc){
 		 // Get image list from the server
 		$.ajax({
-			url: "/image_list/10/" + page
+			url: "/image_list/"+ images_per_page + "/" + page
 		}).success(function(data, code){
 			imagesList = data["data"];
 			imagesCount = data["_meta"].images_count;
 			successFunc(imagesList);
 			return
-		});
+		}).error(function(){
+			 bottom = true;
+			 return
+		})
 
 	};
 
 	function appendIndexImages(imageData){
 			for(var count = 0; count < imageData.length; count +=1){
+				var i = count + 1
 				url = imageData[count].thumb;
 				title = imageData[count].title;
 				el = Mustache.render(imageItemTemplate, imageData[count])
-				$images_list_container.append(el)
+				var item = document.createElement("div");
+				item.className = "image-container";
+				salvattore["append_elements"]($indexImageGrid.get()[0], [item])
+				item.innerHTML = el;
+				$(item).fadeIn("fast");
 			}
 		 
 	}
