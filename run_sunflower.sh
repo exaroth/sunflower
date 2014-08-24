3493e2c8b95ccc774748bc4b0dd4e589ba5538ed
Konrad Wąsowicz <exaroth@gmail.com>
2014-08-24 02:49:02 +0200

    added user to run_sunflower script

--- run_sunflower.sh
+++ run_sunflower.sh
@@ -6,6 +6,7 @@ SOCKET_FILE=/Users/konrad/Repositories/sunflower/sunflower/run/gunicorn.sock
 NUM_WORKERS=3
 DJANGO_SETTINGS_MODULE=sunflower.settings
 DJANGO_WSGI_MODULE=sunflower.wsgi
+USER=root
 
 echo "Running sunflower as `whoami`"
 
@@ -18,6 +19,7 @@ test -d $RUNDIR || mkdir -p $RUNDIR
 
 exec sudo ../bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
 	--name $APP_NAME \
+	--user $USER \
 	--bind unix:$SOCKET_FILE \
 	--workers $NUM_WORKERS \
 	--log-level=debug
